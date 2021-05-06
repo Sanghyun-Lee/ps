c19d23ec6b82604c388068c2f906c844c5df385d
sanghyun lee <sh.lee0404@gmail.com>
2021-05-06 23:09:08 +0900

    [Algospot] MEASURETIME - complete

---
+++ MEASURETIME/measuretime.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct FenwickTree {
+    vector<int> tree;
+    FenwickTree(int n) : tree(n + 1) {}
+
+    int sum (int pos) {
+        pos++;
+        int ret = 0;
+
+        while (pos > 0) {
+            ret += tree[pos];
+            pos &= (pos - 1);
+        }
+        return ret;
+    }
+
+    void add (int pos, int val) {
+        pos++;
+        while (pos < tree.size()) {
+            tree[pos] += val;
+            pos += (pos & - pos);
+        }
+    }
+};
+
+long long countMoves (const vector<int>& A) {
+    FenwickTree tree(1000000);
+    long long ret = 0;
+
+    for (int i = 0; i < A.size(); i++) {
+        ret += tree.sum(999999) - tree.sum(A[i]);
+        tree.add(A[i], 1);
+    }
+
+    return ret;
+}
+
+int main() {
+    int testCase;
+    cin >> testCase;
+
+    while (testCase--) {
+        int n;
+        cin >> n;
+        vector<int> array(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            cin >> array[i];
+        }
+
+        cout << countMoves(array) << endl;
+    }
+    return 0;
+}
