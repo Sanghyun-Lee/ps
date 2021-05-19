b565f44543f6f3ce4a713326ddd9abe5abf7ca24
sanghyun lee <sh.lee0404@gmail.com>
2021-05-20 00:08:25 +0900

    [LeetCode] LargestSumAfterKNegations - complete

---
+++ leetcode/LargestSumAfterKNegations/LargestSumAfterKNegations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int> > pq;
+ 
+        for(int i = 0; i < nums.size(); i++)
+            pq.push(nums[i]);
+        
+        while(k-- > 0) {
+            int temp = pq.top();
+            pq.pop();
+            pq.push(-temp);
+        }
+        
+        int sum = 0;
+        while(!pq.empty()) {
+            sum += pq.top();
+            pq.pop();
+        }
+
+        return sum;
+    }
+};
