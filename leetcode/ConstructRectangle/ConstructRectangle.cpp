0bc97c71d5727144f6b9a1de7873af03e18162e1
sanghyun lee <sh.lee0404@gmail.com>
2021-06-22 23:37:26 +0900

    [LeetCode] 492. ConstructRectangle - complete

---
+++ leetcode/ConstructRectangle/ConstructRectangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        vector<int> ret;
+        int l = area;
+        int w = 1;
+
+        for(int i = area - 1; i > 0; i--) {
+            if(area % i != 0)
+                continue;
+            
+            int newL = i;
+            int newW = area / i;
+            
+            if(newL < newW)
+                break;
+
+            if((l - w) > (newL - newW)) {
+                w = newW;
+                l = newL;
+            }
+        }
+
+        ret.push_back(l);
+        ret.push_back(w);        
+        
+        return ret;
+    }
+};
