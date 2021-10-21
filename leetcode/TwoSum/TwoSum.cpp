bc480a4c664979c36e49a231dfb81dd04265723b
sanghyun lee <sh.lee0404@gmail.com>
2021-10-21 21:38:04 +0900

    [LeetCode] 1.TwoSum - update

--- leetcode/TwoSum/TwoSum.cpp
+++ leetcode/TwoSum/TwoSum.cpp
@@ -3,11 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> results;
         
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i = 0; i < nums.size() - 1; i++) {
             for(int j = i + 1; j < nums.size(); j++) {
                 if(target == (nums[i] + nums[j])) {
                     results.push_back(i);
                     results.push_back(j);
+                    break;
                 }
             }
         }
