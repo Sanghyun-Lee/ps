cc6dc4f9606a4343567d8aac9617a71faff6ad7f
sanghyun lee <sh.lee0404@gmail.com>
2021-11-08 22:18:03 +0900

    [LeetCode] 75. Sort Colors - complete

---
+++ leetcode/SortColors/SortColors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count[3] = {0};
+        for(int i = 0; i < nums.size(); i++) {
+            count[nums[i]]++;
+        }
+
+        nums.clear();
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < count[i]; j++) {
+                nums.push_back(i);
+            }
+        }
+    }
+};
